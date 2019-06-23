da9ee4f1552fcd3b34a0f92f48f7a28d849365cb
Isac Casapu <yitzikc@gmail.com>
2019-06-23 18:15:47 +0100

    Add an admin e-mail

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ fi
 domains=(open-event-server.casapu.pro)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
+email="admin@casapu.pro" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
